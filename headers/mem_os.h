f65d8c3a6c7b64b009fd99ee439988b143310c8c
evsudakov <evsudakov@yandex-team.ru>
2023-10-06 07:28:39 +0200

    malloc first try

--- headers/mem_os.h
+++ headers/mem_os.h
@@ -7,26 +7,35 @@
 #ifndef MEM_OS_H
 #define MEM_OS_H
 
+#define align4(x) (((((x)-1)>>2)<<2)+4)
+#define BLOCK_SIZE sizeof(struct mem_block_s)
+#define GBL_ALLOC_SIZE sizeof(struct mem_allocator_s)
+
 //include stdlib pour definition du type size_t
 #include <stdlib.h>
 
-typedef struct {     
+typedef struct mem_block_s* mem_block_t;
+
+struct mem_block_s{
     size_t size_data;
-    struct mem_free_block_s* next;
-} mem_free_block_s;
+    mem_block_t next;
+
+    int free;
+
+    // pointer to the start of the data
+    char data[1];
+};
 
 
-//Definie la structure the bloc libre
-typedef struct mem_free_block_s mem_free_block_t;
+typedef struct mem_allocator_s* mem_allocator_t;
 
-typedef struct {
-    size_t size_data;
-    struct mem_block_occupied* next;
-} mem_block_occupied;
+typedef mem_block_t (mem_fit_function_t)(mem_block_t , size_t);
 
 struct mem_allocator_s {
-    mem_free_block_s* first_libre;
-    mem_block_occupied* first_occupied;
+    mem_block_t first;
+
+    // i.e. function for searching appropriate block
+    mem_fit_function_t *fit_function;
 };
 /* -----------------------------------------------*/
 /* Interface de gestion de votre allocateur       */
@@ -36,7 +45,6 @@ void mem_init(void);
 
 // Définition du type mem_fit_function_t
 // type des fonctions d'allocation
-typedef mem_free_block_t *(mem_fit_function_t)(mem_free_block_t *, size_t);
 
 // Choix de la fonction d'allocation
 // = choix de la stratégie de l'allocation
--- src/mem.c
+++ src/mem.c
@@ -9,7 +9,9 @@
 #include "mem_os.h"
 #include <assert.h>
 
-struct mem_allocator_s * gbl_allocator = NULL;
+// the very beginning of the Data to be defined later
+mem_allocator_t gbl_allocator = NULL;
+
 //-------------------------------------------------------------
 // mem_init
 //-------------------------------------------------------------
@@ -18,25 +20,37 @@ struct mem_allocator_s * gbl_allocator = NULL;
  * If already init it will re-init.
 **/
 void mem_init() {
-	// ou on place l'info alloc
-	gbl_allocator = mem_space_get_addr();
+    // ou on place l'info alloc
+    gbl_allocator = mem_space_get_addr();
 
-	//Entete premier libre
-	mem_free_block_s* first_free = mem_space_get_addr() + 1;
-	// Calcul size_data du premier bloc
-	int size_data_first_free = mem_space_get_size() - sizeof(mem_free_block_s*) - sizeof(mem_free_block_s*) + sizeof(size_t);
+    //Entete premier libre
+    mem_block_t first_free = (mem_block_t) (gbl_allocator + GBL_ALLOC_SIZE);
 
-	first_free->size_data = size_data_first_free;
-	first_free->next = NULL;
+    //Calcul size_data du premier bloc
+    size_t size_data_first_free = mem_space_get_size() - BLOCK_SIZE - GBL_ALLOC_SIZE;
 
-	gbl_allocator->first_libre = first_free;
-    gbl_allocator->first_occupied = NULL;
+    first_free->size_data = size_data_first_free;
+    first_free->next = NULL;
+    first_free->free = 1;
 
-	printf("%p\n",gbl_allocator);
-	printf("%p\n",first_free);
-	printf("%d\n",size_data_first_free);
+    gbl_allocator->first = first_free;
 
-	
+    //Initialisation de la fonction de recherche par défault
+    mem_set_fit_handler(mem_first_fit);
+
+    printf("%lu\n", (unsigned long) gbl_allocator);
+    printf("%lu\n", (unsigned long) first_free);
+    printf("%zu\n", size_data_first_free);
+}
+
+void split_block(mem_block_t block, size_t size) {
+    mem_block_t new_block;
+    new_block = (mem_block_t) block->data + size;
+    new_block->size_data = block->size_data - size - BLOCK_SIZE;
+    new_block->next = block->next;
+    new_block->free = 1;
+    block->size_data = size;
+    block->next = new_block;
 }
 
 //-------------------------------------------------------------
@@ -46,18 +60,26 @@ void mem_init() {
  * Allocate a bloc of the given size.
 **/
 void *mem_alloc(size_t size) {
-	//TODO: implement
-	assert(! "NOT IMPLEMENTED !");
-    return NULL;
+    mem_block_t b, first_free = NULL;
+    size_t s = align4(size);
+
+    b = gbl_allocator->fit_function(first_free, s);
+    if (b) {
+        if (b->size_data - s >= BLOCK_SIZE) {
+            split_block(b, s);
+        }
+        b->free = 0;
+    }
+
+    return b->data;
 }
 
 //-------------------------------------------------------------
 // mem_get_size
 //-------------------------------------------------------------
-size_t mem_get_size(void * zone)
-{
+size_t mem_get_size(void *zone) {
     //TODO: implement
-	assert(! "NOT IMPLEMENTED !");
+    assert(!"NOT IMPLEMENTED !");
     return 0;
 }
 
@@ -69,7 +91,7 @@ size_t mem_get_size(void * zone)
 **/
 void mem_free(void *zone) {
     //TODO: implement
-	assert(! "NOT IMPLEMENTED !");
+    assert(!"NOT IMPLEMENTED !");
 }
 
 //-------------------------------------------------------------
@@ -77,18 +99,10 @@ void mem_free(void *zone) {
 // mem_show
 //-------------------------------------------------------------}
 void mem_show(void (*print)(void *, size_t, int free)) {
-
-    mem_free_block_s* head_libre = gbl_allocator->first_libre;
-    mem_block_occupied *head_occupied = gbl_allocator->first_occupied;
-
-    while(head_libre || head_occupied) {
-        if (head_occupied && head_libre && (int*) head_libre > (int*) head_occupied) {
-            print(head_occupied, head_occupied->size_data, 0);
-            head_occupied = (mem_block_occupied *) head_occupied->next;
-        } else if (head_libre) {
-            print(head_libre, head_libre->size_data, 1);
-            head_libre = (mem_free_block_s *) head_libre->next;
-        }
+    mem_block_t head = gbl_allocator->first;
+    while (head) {
+        print(head, head->size_data, head->free);
+        head = (mem_block_t) head->next;
     }
 }
 
@@ -96,28 +110,33 @@ void mem_show(void (*print)(void *, size_t, int free)) {
 // mem_fit
 //-------------------------------------------------------------
 void mem_set_fit_handler(mem_fit_function_t *mff) {
-	//TODO: implement
-	assert(! "NOT IMPLEMENTED !");
+    gbl_allocator->fit_function = mff;
 }
 
 //-------------------------------------------------------------
 // Stratégies d'allocation
 //-------------------------------------------------------------
-mem_free_block_t *mem_first_fit(mem_free_block_t *first_free_block, size_t wanted_size) {
-    //TODO: implement
-	assert(! "NOT IMPLEMENTED !");
-	return NULL;
-}
-//-------------------------------------------------------------
-mem_free_block_t *mem_best_fit(mem_free_block_t *first_free_block, size_t wanted_size) {
-    //TODO: implement
-	assert(! "NOT IMPLEMENTED !");
-	return NULL;
+mem_block_t mem_first_fit(mem_block_t first_free_block, size_t wanted_size) {
+
+    mem_block_t b = (mem_block_t) gbl_allocator->first;
+    while (b && !(b->free && b->size_data >= wanted_size)) {
+        first_free_block = b;
+        b = b->next;
+    }
+
+    return b;
 }
 
 //-------------------------------------------------------------
-mem_free_block_t *mem_worst_fit(mem_free_block_t *first_free_block, size_t wanted_size) {
+mem_block_t mem_best_fit(mem_block_t first_free_block, size_t wanted_size) {
     //TODO: implement
-	assert(! "NOT IMPLEMENTED !");
-	return NULL;
+    assert(!"NOT IMPLEMENTED !");
+    return NULL;
+}
+
+//-------------------------------------------------------------
+mem_block_t mem_worst_fit(mem_block_t first_free_block, size_t wanted_size) {
+    //TODO: implement
+    assert(!"NOT IMPLEMENTED !");
+    return NULL;
 }
